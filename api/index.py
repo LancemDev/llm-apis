3fd25f5595fb491b72babcf35c8a423fe887a92f
LancemDev <munyaolance1@gmail.com>
2024-02-08 09:22:58 +0300

    Fix: y

--- api/index.py
+++ api/index.py
@@ -18,6 +18,7 @@ input_names = {
     'license': 'dv_license',
 }
 
+# Constants for different categories
 agriculture = 189
 business = 217
 education = 218
@@ -28,26 +29,33 @@ hospitality = 216
 humanities = 188
 pure_sciences = 215
 
-
+# Example URL for engineering category
 full_url_example = base_url + str(engineering)
 
+# Headers for HTTP requests
 headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': 'JSESSIONID=3A2F3B4E3B0F9A',
-        'Host': 'ir.mksu.ac.ke',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Origin": "https://ir.mksu.ac.ke",
-        "Referer": "https://ir.mksu.ac.ke/handle/123456780/219",
-        "Upgrade-Insecure-Requests": "1",
-
-    }
-
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie': 'JSESSIONID=3A2F3B4E3B0F9A',
+    'Host': 'ir.mksu.ac.ke',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Origin": "https://ir.mksu.ac.ke",
+    "Referer": "https://ir.mksu.ac.ke/handle/123456780/219",
+    "Upgrade-Insecure-Requests": "1",
+}
 
 # Function to fill and submit the form for a single document
 def submit_document(doc):
-    return jsonify({'message': 'Document submitted!', 'status': 'success'})
+    """
+    Submits a document by filling and submitting the form.
+
+    Args:
+        doc (str): The document to submit.
+
+    Returns:
+        dict: A JSON response indicating the status of the submission.
+    """
     # Form data for the document
     form_data = {
         'dv_contributor_author_last': 'Machakos University',
@@ -58,8 +66,7 @@ def submit_document(doc):
         'decision': 'on',
     }
 
-    # form_data['additional_field'] = 'additional_value'
-    # get title of the doc from the uploaded file
+    # Get title of the doc from the uploaded file
     form_data['dc_title'] = doc.split('.')[0]
 
     # Submit the form
@@ -76,22 +83,51 @@ def submit_document(doc):
 # Route for testing
 @app.route('/test')
 def test():
+    """
+    A test route to check if the URLs are working.
+
+    Returns:
+        str: A test message indicating that the URLs are working.
+    """
     return "Urls should be working!"
 
 @app.route('/test-uploads')
 def test_uploads():
+    """
+    A route to test file uploads.
+
+    Returns:
+        str: The rendered template for the file upload page.
+    """
     return render_template('/home/index.html')
 
 @app.route('/')
 def index():
-    # return render_template('test/index.html')
+    """
+    The main route of the application.
+
+    Returns:
+        str: A welcome message.
+    """
     return "Welcome to the Machakos University IR API!"
 
 @app.route('/x', methods=['GET'])
 def x():
+    """
+    A route for testing.
+
+    Returns:
+        str: The rendered template for the main page.
+    """
     return render_template('main.html')
 
 def save_doc(file):
+    """
+    Saves a document to the uploads directory.
+
+    Args:
+        file (FileStorage): The file to save.
+    """
     upload_dir = Path('uploads')
     upload_path = upload_dir / file.filename
     
@@ -101,11 +137,14 @@ def save_doc(file):
     file.save(upload_path)
     print(f"File {file.filename} saved successfully.")
 
-
-    
-
 @app.route('/submit_documents', methods=['POST'])
 def submit_documents():
+    """
+    Submits multiple documents.
+
+    Returns:
+        str: A message indicating the status of the file processing.
+    """
     uploaded_files = request.files.getlist('documents')
 
     # Process the uploaded files as needed
@@ -113,12 +152,12 @@ def submit_documents():
         print(f"Processing file {i} of {len(uploaded_files)}: {file.filename}")
 
         # Save each file
-        # Let's remove the saving part. It not as important
+        # Let's remove the saving part. It's not as important
         # print(f"Saving file {file.filename}...")
         # save_doc(file)
         # print(f"File {file.filename} saved.")
 
-        # then submit the document
+        # Then submit the document
         print(f"Submitting file {file.filename}...")
         submit_document(file)
         print(f"File {file.filename} submitted.")
@@ -128,16 +167,26 @@ def submit_documents():
 
 @app.route('/login', methods=['GET'])
 def login_get():
+    """
+    A route to display the login page.
+
+    Returns:
+        str: The rendered template for the login page.
+    """
     return render_template('home/login.html')
 
 session = requests.Session()
 
 @app.route('/login', methods=['POST'])
 def login_post():
-    # return "Login successful!"
+    """
+    Handles the login form submission.
+
+    Returns:
+        str: A message indicating the status of the login.
+    """
     email = request.form.get('email')
     password = request.form.get('password')
-
 
     password_input_name = "login_password"
     email_input_name = "login_email"
@@ -152,7 +201,7 @@ def login_post():
     response = session.post(login_url, data=login_data, headers=headers)
     if not response.ok:
         return "Login failed.....\nEmail: " + email + "\nPassword: " + password
-    # redirect to /test-uploads
+    # Redirect to /test-uploads
     return "Login successful!.....\nEmail: " + email + "\nPassword: " + password
     
 
