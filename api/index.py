7d79e10da328140f8e84c3e7b1066ee0b45a416c
LancemDev <munyaolance1@gmail.com>
2024-02-12 10:17:12 +0300

    Feat: y g test

--- api/index.py
+++ api/index.py
@@ -252,8 +252,19 @@ def fetch_sth():
     Returns:
         str: The response from the base URL.
     """
-    response = session.get('http://ir.mksu.ac.ke/handle/123456780/221/submit')
-    return response.text    
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cookie': 'amp_6e403e=y7amtQ0LsgPgK8SnXM3eGG...1hearcqsl.1hearcqsl.0.0.0; _ga=GA1.1.100642076.1700651316; _ga_NF2FGF1NDQ=GS1.1.1700651315.1.1.1700651468.60.0.0; JSESSIONID=1D8E43C16A80AAF0F80AC401E24CF43B',
+        'Host': 'ir.mksu.ac.ke',
+        'Referer': 'http://ir.mksu.ac.ke/handle/123456780/187',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    }
+    response = requests.get('http://ir.mksu.ac.ke/handle/123456780/221/submit', headers=headers)
+    return response.text  
 
 if __name__ == "__main__":
     app.run(debug=True)
