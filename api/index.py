8930dc0e8c19e3ced0810f02b5430694b30ac551
LancemDev <munyaolance1@gmail.com>
2024-02-12 12:14:13 +0300

    Feat: y n test

--- api/index.py
+++ api/index.py
@@ -58,9 +58,21 @@ def submit_document(file):
         'dc_langauge': 'en', 
     }
 
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cookie': 'amp_6e403e=y7amtQ0LsgPgK8SnXM3eGG...1hearcqsl.1hearcqsl.0.0.0; _ga=GA1.1.100642076.1700651316; _ga_NF2FGF1NDQ=GS1.1.1700651315.1.1.1700651468.60.0.0; JSESSIONID=1D8E43C16A80AAF0F80AC401E24CF43B',
+        'Host': 'ir.mksu.ac.ke',
+        'Referer': 'http://ir.mksu.ac.ke/handle/123456780/187',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+    }
+
     # Get title of the doc from the uploaded file
     form_data['dc_title'] = file.filename.split('.')[0]
-    form_data['file'] = (file.filename, file.read())
+    form_data['file'] = file    
 
     # URL to submit the form
     target_url = 'http://ir.mksu.ac.ke/handle/123456780/219/submit'
@@ -73,9 +85,8 @@ def submit_document(file):
     # Submit the form
     try:
         with requests.Session() as session:
-            post = session.post(login_url, data=credentials, headers=headers)
-            response = session.post(target_url, data=form_data, headers=headers)
-            # response = session.get(target_url, headers=headers)
+            post = session.post(login_url, data=credentials)
+            response = session.post(target_url, data=form_data)
     except requests.exceptions.Timeout:
         print(f"Timeout occurred while submitting document '{file.filename}'")
         return None
@@ -96,7 +107,9 @@ def submit_document(file):
     else:
         print(f"Failed to submit document '{file.filename}'. Status code: {response.status_code}")
 
-    return response@app.route('/test')
+    return response
+
+@app.route('/test')
 def test():
     """
     A test route to check if the URLs are working.
