377542fd5821c5825c5c59a06c3921a0fa2390b6
Lance Munyao <munyaolance1@gmail.com>
2024-04-22 04:26:35 +0300

    Feat: Routes update

--- api/index.py
+++ api/index.py
@@ -78,11 +78,11 @@ def homepage():
     return redirect('/login')
   return render_template('homepage.html')
 
-app.route('/landing')
+app.route('/landing', methods=['GET'])
 def landing():
   return render_template('landing.html')
 
-app.route('/update-account')
+app.route('/update-account', methods=['GET'])
 def update_account():
   return render_template('update-account.html')
 
