77ef4f2eb857622cbf7664b9a3fa5a759dcada20
Lance Munyao <munyaolance1@gmail.com>
2024-04-23 07:23:03 +0300

    Feat: Homepage 8

--- api/index.py
+++ api/index.py
@@ -48,7 +48,7 @@ def gregorian_sarcasm():
 
 @app.route('/')
 def index():
-  return "Welcome to this service. If you're not Lance you might be lost."
+  return "Hello"
 
 @app.route('/maps')
 def maps():
